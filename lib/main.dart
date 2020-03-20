9fe0097e24b4f3c734627d806ed56937ecc60eab
jgrt04 <jgrt04@comcast.net>
2020-03-20 14:27:34 -0700

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.deepPurple),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -47,26 +38,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-      TextStyle(fontSize: 40, fontWeight: FontWeight.normal);
+      TextStyle(fontSize: 50, fontWeight: FontWeight.normal);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
-      'Index 0: Home',
+      'Calendar',
       style: optionStyle,
     ),
     Text(
-      'Index 1: Today',
+      'Add Mood',
       style: optionStyle,
     ),
     Text(
-      'Index 2: Trends',
+      'Trends',
       style: optionStyle,
     ),
   ];
 
+  int count = 0;
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      count++;
     });
+    //print("button clicked $count times");
   }
 
   @override
@@ -81,12 +75,12 @@ class _MyHomePageState extends State<MyHomePage> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
+            icon: Icon(Icons.calendar_today),
+            title: Text('Calendar'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            title: Text('Today'),
+            icon: Icon(Icons.add),
+            title: Text('Add Mood'),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.timeline),
