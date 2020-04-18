2c9b7d0b2cbb68e1d34a7b6bfa259bc3144751ab
Jeane <jgrt04@gmail.com>
2020-04-18 00:40:37 -0700

    Added AddMood button to calendar state

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ A Flutter application that allows users to log in how they feel during the day t
 
 ## Features
 
-- Adding a mood
-    -Users are welcomed by a calendar that displays todays date.
-    -Sliders are then presented with corresponding moods such as happy, sad, angry, or anxious.
-    -Each slider has a range of 0 - 10, which displays the strength of the mood. 0 being the lowest and 10 being the highest.
-    -Once each mood has been adjusted, the user can log the data back to the calendar.
+- Adding a mood\
+    -Users are welcomed by a calendar that displays today's date.\
+    -Sliders are then presented with corresponding moods such as happy, sad, angry, or anxious.\
+    -Each slider has a range of 0 - 10, which displays the strength of the mood. 0 being the lowest and 10 being the highest.\
+    -Once each mood has been adjusted, the user can log the data back to the calendar.\
 
-- Displaying moods
+- Displaying moods\
     -The different moods also have corresponding colors to them. The mood with the highest strength for the day will be
-        displayed on a calendar along with its color.
-    -These colors will also be added to a display that shows all logs throughout the year.
+        displayed on a calendar along with its color.\
+    -These colors will also be added to a display that shows all logs throughout the year.\
     -Daily line graphs will also be shown, allowing the user to observe trends in their moods.
 
--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,11 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-//import './addMood.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './addMood.dart';
 
-class CalendarPage extends StatefulWidget{
-
+class CalendarPage extends StatefulWidget {
   DateTime datePicked;
-  CalendarPage({Key key, @required this.datePicked}) : super(key : key);
+  CalendarPage({Key key, @required this.datePicked}) : super(key: key);
 
   @override
   CalendarState createState() => CalendarState(datePicked);
@@ -14,38 +16,114 @@ class CalendarPage extends StatefulWidget{
 class CalendarState extends State<CalendarPage> {
   CalendarController _controller;
   DateTime datePicked;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+  TextEditingController _eventController;
+  SharedPreferences prefs;
+
   CalendarState(this.datePicked);
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _controller = CalendarController(
-    );
+    _controller = CalendarController();
+    _eventController = TextEditingController();
+    _events = {};
+    _selectedEvents = [];
+    initPrefs();
+  }
+
+  initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _events = Map<DateTime, List<dynamic>>.from(
+          decodeMap(json.decode(prefs.getString("events") ?? "{}")));
+    });
+  }
+
+  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+    Map<String, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+    Map<DateTime, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TableCalendar(
-                calendarStyle: CalendarStyle(
-                    todayColor: Colors.amber[800],
-                    selectedColor: Colors.amber,
-                    todayStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.white,
-                    )),
-                calendarController: _controller,
-              ),
-//              Text(this.datePicked.toString(),
-//              ),
-            ],
-          )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              events: _events,
+              calendarStyle: CalendarStyle(
+                  todayColor: Colors.black12,
+                  selectedColor: Colors.amber,
+                  todayStyle: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 18,
+                    color: Colors.white,
+                  )),
+              calendarController: _controller,
+              onDaySelected: (date, events) {
+                setState(() {
+                  _selectedEvents = events;
+                });
+              },
+            ),
+            ..._selectedEvents.map((event) => ListTile(
+                  title: Text(event),
+                )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Add Mood'),
+        icon: Icon(Icons.add),
+        onPressed: _showAddDialog,
+      ),
     );
+  }
+
+  _showAddDialog() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: _eventController,
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Save"),
+                  onPressed: () {
+                    if (_eventController.text.isEmpty) return;
+                    setState(() {
+                      if (_events[_controller.selectedDay] != null) {
+                        _events[_controller.selectedDay]
+                            .add(_eventController.text);
+                      } else {
+                        _events[_controller.selectedDay] = [
+                          _eventController.text
+                        ];
+                      }
+                      prefs.setString(
+                          "events", json.encode(encodeMap(_events)));
+                      _eventController.clear();
+                      Navigator.pop(context);
+                    });
+                  },
+                )
+              ],
+            ));
   }
 }
